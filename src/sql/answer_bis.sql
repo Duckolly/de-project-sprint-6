3e3735d6e32d1aa73b3326d2bee75df2091ffb14
Duckolly <s.shvetsov@pravo.tech>
2024-05-23 18:08:47 +0400

    done +l_user_message+l_groups_dialog

--- src/sql/answer_bis.sql
+++ src/sql/answer_bis.sql
@@ -1,28 +1,21 @@
-WITH user_group_log AS (
-    SELECT 
-        hg.hk_group_id,
-        COUNT(DISTINCT sa.user_id_from) AS cnt_added_users
-    FROM STV2024050744__DWH.s_auth_history AS sa
-    LEFT JOIN STV2024050744__DWH.h_groups AS hg ON sa.hk_l_user_group_activity = hg.hk_group_id
-    WHERE sa.event = 'add'
-    GROUP BY hg.hk_group_id
-    ORDER BY MIN(hg.registration_dt)
-    LIMIT 10
-),
-user_group_messages AS (
-    SELECT 
-        hg.hk_group_id,
-        COUNT(DISTINCT sa.hk_user_id) AS cnt_users_in_group_with_messages
-    FROM STV2024050744__DWH.l_user_group_activity AS sa
-    LEFT JOIN STV2024050744__DWH.h_groups AS hg ON sa.hk_l_user_group_activity = hg.hk_group_id
-    GROUP BY hg.hk_group_id
+with user_group_log as (
+    select hg.hk_group_id, hg.registration_dt, count(DISTINCT luga.hk_user_id) cnt_added_users
+    from STV2024050744__DWH.h_groups hg 
+    left join STV2024050744__DWH.l_user_group_activity luga on luga.hk_group_id = hg.hk_group_id
+    left join STV2024050744__DWH.s_auth_history sah on sah.hk_l_user_group_activity = luga.hk_l_user_group_activity 
+    where sah.event = 'add'
+    group by hg.hk_group_id, hg.registration_dt
+    order by hg.registration_dt 
+    limit 10
 )
-
-SELECT 
-    ugl.hk_group_id,
-    ugl.cnt_added_users,
-    COALESCE(ugm.cnt_users_in_group_with_messages, 0) AS cnt_users_in_group_with_messages,
-    (COALESCE(ugm.cnt_users_in_group_with_messages, 0) / NULLIF(ugl.cnt_added_users, 0)) AS group_conversion
-FROM user_group_log AS ugl
-LEFT JOIN user_group_messages AS ugm ON ugl.hk_group_id = ugm.hk_group_id
-ORDER BY group_conversion DESC;
+, user_group_messages as (
+    select lgd.hk_group_id, count(distinct lum.hk_user_id) cnt_users_in_group_with_messages
+     from STV2024050744__DWH.l_groups_dialogs lgd
+     left join STV2024050744__DWH.l_user_message lum on lum.hk_message_id = lgd.hk_message_id
+     group by lgd.hk_group_id
+)
+select ugl.hk_group_id, ugl.cnt_added_users, ugm.cnt_users_in_group_with_messages, 
+ugm.cnt_users_in_group_with_messages / ugl.cnt_added_users group_conversion
+from user_group_log as ugl
+left join user_group_messages as ugm on ugl.hk_group_id = ugm.hk_group_id
+order by ugm.cnt_users_in_group_with_messages / ugl.cnt_added_users desc
